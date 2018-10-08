5fc6aee55310b075ceb5c15754d119f460a5d445
Katarzyna Kaldunska <k.kaduska@gmail.com>
2018-10-08 20:50:03 +0200

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const {mongoose} = require('./db/mongoose');
 const OmdbClient = require('./omdbClient');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
